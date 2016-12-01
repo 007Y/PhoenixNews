91f1e28dfcbd5cfc2ab543af44c11c18d5ab001d
李冬 <2500334604@qq.com>
2016-12-01 15:27:35 +0800

    no message

--- News/News/ViewController.m
+++ News/News/ViewController.m
@@ -17,7 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor redColor]
-    ;    // Do any additional setup after loading the view, typically from a nib.
+    ;
+//    234567890
+    // Do any additional setup after loading the view, typically from a nib.
 }
 
 

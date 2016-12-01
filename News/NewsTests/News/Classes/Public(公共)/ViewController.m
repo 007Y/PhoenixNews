6fd5811363158fcb0f58dbb839fd0c9726af4c3a
wyzc <975997359@qq.com>
2016-12-01 16:58:17 +0800

    4.58

---
+++ News/NewsTests/News/Classes/Public(公共)/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  News
+//
+//  Created by wyzc on 16/12/1.
+//  Copyright © 2016年 wyzc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor]
+    ;
+    UIView * view = [[UIView alloc]init];
+//    234567890
+    //2222222222
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
